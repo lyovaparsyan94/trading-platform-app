1f788677d0e1a2b1412a349b445ee35a3b6f8fbd
Lyova PArsyan <parsyan.lyova@gmail.com>
2024-05-24 17:45:46 +0400

    update repositories

--- src/apps/trading_platform/repositories/django_orm_deal_repository.py
+++ src/apps/trading_platform/repositories/django_orm_deal_repository.py
@@ -1,25 +1,15 @@
-from typing import List
 from src.apps.trading_platform.models import DealSettings
 from src.apps.trading_platform.repositories.i_deal_repository import IDealRepository
 
 
 class DjangoORMDealRepository(IDealRepository):
-    def get_deal_by_id(self, deal_id: int) -> DealSettings:
+    def get_deal_by_id_or_none(self, deal_id: int) -> DealSettings | None:
         """Fetch a deal from the database by its ID."""
         try:
             return DealSettings.objects.get(id=deal_id)
         except DealSettings.DoesNotExist:
             return None
 
-    def get_all_deals(self) -> List[DealSettings]:
+    def get_all_deals(self) -> list[DealSettings]:
         """Fetch all deals from the database."""
         return list(DealSettings.objects.all())
-
-
-def example_usage():
-    deal_repository = DjangoORMDealRepository()
-    all_deals = deal_repository.get_all_deals()
-    print(f"All deals: {all_deals}")
-
-
-example_usage()
--- src/apps/trading_platform/repositories/i_deal_repository.py
+++ src/apps/trading_platform/repositories/i_deal_repository.py
@@ -1,15 +1,14 @@
 from abc import ABC, abstractmethod
-from typing import List
 from src.apps.trading_platform.models import DealSettings
 
 
 class IDealRepository(ABC):
     @abstractmethod
-    def get_deal_by_id(self, deal_id: int) -> DealSettings:
+    def get_deal_by_id_or_none(self, deal_id: int) -> DealSettings | None:
         """Fetch a deal from the database by its ID."""
-        raise NotImplementedError("Method get_deal_by_id() must be implemented")
+        raise NotImplementedError
 
     @abstractmethod
-    def get_all_deals(self) -> List[DealSettings]:
+    def get_all_deals(self) -> list[DealSettings]:
         """Fetch all deals from the database."""
-        raise NotImplementedError("Method get_all_deals() must be implemented")
+        raise NotImplementedError
