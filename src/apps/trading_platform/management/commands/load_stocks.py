ace1905eeec1eab87e56c19d82b5cbc63c3d2be4
egorverkhozin <nonamers1336@gmail.com>
2024-05-27 18:02:10 +0300

    Added new command for import stocks

---
+++ src/apps/trading_platform/management/commands/load_stocks.py
@@ -0,0 +1,24 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand
+
+from src.apps.trading_platform.models import Stock
+
+
+class Command(BaseCommand):
+    help = 'Load stocks from a CSV file'
+
+    def handle(self, *args: str, **kwargs: str) -> None:
+        """Handle command execution."""
+        csv_file_path = os.path.join("src", "apps", "trading_platform", "static", "stocks.csv")
+
+        with open(csv_file_path, mode='r', newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                name = row['Name']
+                indicator, created = Stock.objects.get_or_create(name=name)
+                if created:
+                    self.stdout.write(self.style.SUCCESS(f'Indicator "{name}" added successfully.'))
+                else:
+                    self.stdout.write(self.style.WARNING(f'Indicator "{name}" already exists.'))
